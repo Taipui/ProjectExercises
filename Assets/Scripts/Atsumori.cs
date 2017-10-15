00853e22516385d4f5aa65ac2d86c5548d06f5e6
Taipui <taipuiupiat@gmail.com>
2017-10-15 16:00:53 +0900

    おまけ2。[Fixed]

---
+++ Assets/Scripts/Atsumori.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Atsumori : MonoBehaviour
+{
+	[SerializeField]
+	Transform ImgTfm;
+
+	void Start ()
+	{
+		ImgTfm.localScale = Vector3.zero;
+
+		StartCoroutine("atsumoriLoop");
+	}
+
+	IEnumerator atsumoriLoop()
+	{
+		while (true) {
+			yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
+
+			ImgTfm.DOScale(
+				Vector3.one / 2,
+				0.2f
+				).SetEase(Ease.InElastic);
+
+			yield return new WaitForSeconds(3.0f);
+			ImgTfm.localScale = Vector3.zero;
+		}
+	}
+}
+
