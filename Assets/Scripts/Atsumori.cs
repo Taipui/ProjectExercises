f6de7f0e581b355ef575dc930e7ee59c89e0e765
Taipui <taipuiupiat@gmail.com>
2017-10-15 16:05:11 +0900

    おまけ2を編集。[Fixed]

--- Assets/Scripts/Atsumori.cs
+++ Assets/Scripts/Atsumori.cs
@@ -8,6 +8,8 @@ public class Atsumori : MonoBehaviour
 {
 	[SerializeField]
 	Transform ImgTfm;
+	[SerializeField]
+	AudioSource AudioSource;
 
 	void Start ()
 	{
@@ -21,12 +23,14 @@ public class Atsumori : MonoBehaviour
 		while (true) {
 			yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
 
+			AudioSource.Play();
+
 			ImgTfm.DOScale(
 				Vector3.one / 2,
 				0.2f
 				).SetEase(Ease.InElastic);
 
-			yield return new WaitForSeconds(3.0f);
+			yield return new WaitForSeconds(2.5f);
 			ImgTfm.localScale = Vector3.zero;
 		}
 	}
