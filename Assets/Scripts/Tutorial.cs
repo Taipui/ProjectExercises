66d8c8de0f6cc08d775fc8fa67be84025a733143
Taipui <taipuiupiat@gmail.com>
2017-10-15 15:17:36 +0900

    プレイヤーが看板に近づいている時にEnterキーを押すとチュートリアル用のメッセージのポップアップが表示されるよう変更。[Fixed]

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
+using DG.Tweening;
 
 /// <summary>
 /// チュートリアルに関するクラス
@@ -20,20 +21,37 @@ public class Tutorial : MonoBehaviour
 	[SerializeField]
 	GameObject PlayerMes;
 
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップのTransform
+	/// </summary>
+	[SerializeField]
+	Transform TutorialMesPop;
+
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップを表示する前のimeScale
+	/// </summary>
+	float prevTimeScale;
+
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップを拡大/縮小させるTween
+	/// </summary>
+	Tweener popTween;
+
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップが開いているかどうか
+	/// </summary>
+	bool isOpen;
+
 	void Start ()
 	{
 		col = GetComponent<BoxCollider>();
 		PlayerMes.SetActive(false);
+		TutorialMesPop.localScale = Vector3.zero;
+		isOpen = false;
 
 		col.OnTriggerEnterAsObservable().Where(colObj => colObj.gameObject.tag == "Player")
 			.Subscribe(_ => {
 				PlayerMes.SetActive(true);
-			})
-			.AddTo(this);
-
-		col.OnTriggerStayAsObservable().Where(colObj => colObj.gameObject.tag == "Player" && !!Input.GetKeyDown(KeyCode.Return))
-			.Subscribe(_ => {
-				Debug.Log("hoge");
 			})
 			.AddTo(this);
 
@@ -42,6 +60,43 @@ public class Tutorial : MonoBehaviour
 				PlayerMes.SetActive(false);
 			})
 			.AddTo(this);
+
+		this.UpdateAsObservable().Where(x => !!PlayerMes.activeSelf && !!Input.GetKeyDown(KeyCode.Return))
+			.Subscribe(_ => {
+				if (!isOpen) {
+					openPop();
+				} else {
+					closePop();
+				}
+				isOpen = !isOpen;
+			})
+			.AddTo(this);
+	}
+
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップの表示
+	/// </summary>
+	void openPop()
+	{
+		prevTimeScale = Time.timeScale;
+		Time.timeScale = 0.0f;
+		TutorialMesPop.DOScale(
+			Vector3.one,
+			0.5f
+			).SetUpdate(true);
+	}
+
+	/// <summary>
+	/// チュートリアル用のメッセージのポップアップの非表示
+	/// </summary>
+	void closePop()
+	{
+		TutorialMesPop.DOScale(
+			Vector3.zero,
+			0.5f
+			).SetUpdate(true)
+			.OnComplete(() => {
+				Time.timeScale = prevTimeScale;
+			});
 	}
 }
-
