0965d320c4b593c823de5913e567d5e3ca99a454
Taipui <taipuiupiat@gmail.com>
2017-10-25 17:51:58 +0900

    ランダムにタイトルシーンが選ばれるよう変更。[Fixed]

---
+++ Assets/Scripts/SceneSelecter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// どのタイトルシーンにするかをランダムで選ぶクラス
+/// </summary>
+public class SceneSelecter : MonoBehaviour
+{
+	void Start ()
+	{
+		switch (Random.Range(0, 2)) {
+			default:
+				Debug.Log("エラー");
+				return;
+			case 0:
+				SceneManager.LoadScene("Title1");
+				break;
+			case 1:
+				SceneManager.LoadScene("Title2");
+				break;
+			case 2:
+				break;
+		}		
+	}
+}
+
