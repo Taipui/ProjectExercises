6c4a9c9d390f8f2c4d80058deb83e14114ee7b13
Taipui <taipuiupiat@gmail.com>
2017-11-01 17:06:09 +0900

    ステージ2の坂を障害物から雪に変更。[Fixed]

--- Assets/Scripts/SmallGroundCreater.cs
+++ Assets/Scripts/SmallGroundCreater.cs
@@ -28,12 +28,39 @@ public class SmallGroundCreater : MonoBehaviour
 	/// </summary>
 	Vector2 groundChipeScale;
 
+	/// <summary>
+	/// 生成オプション
+	/// </summary>
+	enum CreateOption
+	{
+		/// <summary>
+		/// 通常(起伏なし)
+		/// </summary>
+		Normal,
+		/// <summary>
+		/// 坂
+		/// </summary>
+		Slope
+	}
+
+	[SerializeField]
+	CreateOption CurrentCreateOption;
+
 	void Awake()
 	{
 		groundChipeScale = GroundChip.transform.localScale;
 		groundChipeScale /= 100;
 
-		create();
+		switch (CurrentCreateOption) {
+			default:
+				return;
+			case CreateOption.Normal:
+				createNormal();
+				break;
+			case CreateOption.Slope:
+				createSlope();
+				break;
+		}
 	}
 
 	void Start ()
@@ -41,19 +68,37 @@ public class SmallGroundCreater : MonoBehaviour
 	}
 
 	/// <summary>
-	/// 地面を作成する
+	/// 平たい地面を作成する
 	/// </summary>
-	void create()
+	void createNormal()
 	{
 		var wPos = 0.0f;
 		var hPos = 0.0f;
 		for (var w = 0; w < WNum; ++w) {
 			for (var h = 0; h < HNum; ++h) {
-				var obj = Instantiate(GroundChip, new Vector3(wPos, hPos), Quaternion.identity);
-				obj.transform.SetParent(transform);
+				var go = Instantiate(GroundChip, transform);
+				go.transform.localPosition = new Vector3(wPos, hPos);
 				hPos += groundChipeScale.y;
 			}
 			hPos = 0.0f;
+			wPos += groundChipeScale.x;
+		}
+	}
+
+	/// <summary>
+	/// 坂を作成する
+	/// </summary>
+	void createSlope()
+	{
+		var wPos = 0.0f;
+		var hPos = 0.0f;
+		for (var w = 0; w < WNum; ++w) {
+			for (var h = 0; h < HNum; ++h) {
+				var go = Instantiate(GroundChip, transform);
+				go.transform.localPosition = new Vector3(wPos, hPos);
+				hPos += groundChipeScale.y;
+			}
+			hPos = groundChipeScale.y * w * 6;
 			wPos += groundChipeScale.x;
 		}
 	}
