6e711257f0cfcb349cba7e69bddf23bb1e43960c
Taipui <taipuiupiat@gmail.com>
2018-01-10 11:41:04 +0900

    ステージ5のボス前の障害物を変更。[Fixed]

---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// マテリアルのタイリング
+/// </summary>
+public class Tiling : MonoBehaviour
+{
+	void Start ()
+	{
+		GetComponent<Renderer>().material.mainTextureScale = new Vector2(0.5f, 0.5f);
+	}
+}
