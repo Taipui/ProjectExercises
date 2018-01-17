72429dda2b2df6f84aea11aed4b9fa1c0a90e759
Taipui <taipuiupiat@gmail.com>
2018-01-17 09:49:17 +0900

    スタッフロールシーンで、画面外に飛んだ雪弾で先の文字が消えてしまう不具合を修正。[Fixed]

---
+++ Assets/Scripts/StaffRoll/StaffRollBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+/// <summary>
+/// スタッフロール用のキャラクターが発射した弾に関するクラス
+/// </summary>
+public class StaffRollBullet : Bullet
+{
+	void Start ()
+	{
+		this.UpdateAsObservable().Where(x => transform.position.x > Camera.main.transform.position.x + 10.0f)
+			.Subscribe(_ => {
+				Destroy(gameObject);
+			})
+			.AddTo(this);
+	}
+}
