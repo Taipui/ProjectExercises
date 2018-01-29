a5ed08cc6e50f32fd61fd343aa9d0659d13dcfe4
Taipui <taipuiupiat@gmail.com>
2018-01-29 22:21:02 +0900

    Escでいつでもゲームが終われるよう変更。[Fixed]

---
+++ Assets/Scripts/AlwaysEscape.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+/// <summary>
+/// いつでもEscでゲームが終われるようにするためのクラス
+/// </summary>
+public class AlwaysEscape : MonoBehaviour
+{
+	void Start ()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		this.UpdateAsObservable().Where(x => !!Input.GetKeyDown(KeyCode.Escape))
+			.Subscribe(_ => {
+				Application.Quit();
+			})
+			.AddTo(this);
+	}
+}
